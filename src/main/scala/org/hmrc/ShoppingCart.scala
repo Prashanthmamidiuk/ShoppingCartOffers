6da0545b5fa1dfd4a9924964a55ff2f6397af96f
PrashanthMamidiUK <Prashanth.mamidi.uk@gmail.com>
2022-09-02 17:25:11 +0100

    shopping cart with offers

--- src/main/scala/org/hmrc/ShoppingCart.scala
+++ src/main/scala/org/hmrc/ShoppingCart.scala
@@ -1,14 +1,25 @@
 package org.hmrc
 
 class ShoppingCart {
-  def compute(shoppingList: List[String]) : String = {
-    val totalCost = shoppingList.map {
+  def checkout(shoppingList: List[String]) : String = {
+    val totalCost = shoppingList.map(getPrice).sum.toDouble
+    val offers = offerPriceFor(shoppingList, "Apple", 2) +
+      offerPriceFor(shoppingList, "Orange", 3)
+
+    s"£${(totalCost - offers) / 100}"
+  }
+
+  private def getPrice(item: String) = {
+    item match {
       case "Apple" => 60
       case "Orange" => 25
       case item => throw new IllegalArgumentException(s"Item ${item} is invalid")
-    }.sum.toDouble
+    }
+  }
 
-    s"£${totalCost/100}"
+  private def offerPriceFor(shoppingList: List[String], fruitType: String, offer: Int) = {
+    val fruitCount = shoppingList.count(_ == fruitType)
+    (fruitCount / offer) * getPrice(fruitType)
   }
 
 }
--- src/test/scala/org/hmrc/ShoppingCartSpec.scala
+++ src/test/scala/org/hmrc/ShoppingCartSpec.scala
@@ -8,34 +8,64 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
   val shoppingCart = new ShoppingCart
 
   "shopping cart" should "calculate the price of items in the basket" in {
-    val result = shoppingCart.compute(List("Apple", "Apple", "Orange", "Apple"))
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Orange", "Apple"))
 
-    result shouldEqual "£2.05"
+    result shouldEqual "£1.45"
   }
 
   it should "calculate the price of basket, given basket has only Apple" in {
-    val result = shoppingCart.compute(List("Apple"))
+    val result = shoppingCart.checkout(List("Apple"))
 
     result shouldEqual "£0.6"
   }
 
   it should "calculate the price of basket, given basket has only Orange" in {
-    val result = shoppingCart.compute(List("Orange"))
+    val result = shoppingCart.checkout(List("Orange"))
 
     result shouldEqual "£0.25"
   }
 
   it should "return 0 if basket is empty" in {
-    val result = shoppingCart.compute(List())
+    val result = shoppingCart.checkout(List())
 
     result shouldEqual "£0.0"
   }
 
   it should "throw exception if basket has invalid item" in {
     val exception = intercept[IllegalArgumentException] {
-      shoppingCart.compute(List("Apple", "Banana", "Orange"))
+      shoppingCart.checkout(List("Apple", "Banana", "Orange"))
     }
     exception.getMessage shouldBe "Item Banana is invalid"
+  }
+
+  "shopping cart with offers" should "apply buy one get one offer, given basket has 2 Apples" in {
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Orange"))
+
+    result shouldEqual "£0.85"
+  }
+
+  it should "apply buy one get one offer, given basket has 5 Apples" in {
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Apple", "Apple", "Apple", "Orange"))
+
+    result shouldEqual "£2.05"
+  }
+
+  it should "apply 3 for 2 offer, given basket has 3 oranges" in {
+    val result = shoppingCart.checkout(List("Apple", "Orange", "Orange", "Orange"))
+
+    result shouldEqual "£1.1"
+  }
+
+  it should "apply 3 for 2 offer, given basket has 5 oranges" in {
+    val result = shoppingCart.checkout(List("Apple", "Orange", "Orange", "Orange", "Orange", "Orange"))
+
+    result shouldEqual "£1.6"
+  }
+
+  it should "apply buy one get one offer and 3 for 2 offer, given basket has 5 apples and 4 oranges" in {
+    val result = shoppingCart.checkout(List("Apple", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange"))
+
+    result shouldEqual "£2.55"
   }
 
 }
